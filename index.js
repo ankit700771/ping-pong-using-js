2981e29c713dc19e0b9fd9de950cce8fa5a496cd
ankit700771 <ankitgupta6564@.com>
2024-07-02 17:01:56 +0530

    balls and handle are move perfectly with the help of arrowkey

---
+++ index.js
@@ -0,0 +1,39 @@
+// DOMContentLoaded => The DOMContentLoaded event fires when the HTML document has been completely parsed
+// and all deferred scripts (<script defer src="…"> and <script type="module">) have downloaded and executed.
+// It doesn't wait for other things like images, subframes, and async scripts to finish loading.
+
+document.addEventListener("DOMContentLoaded", () => {
+  let ball = document.getElementById("ball");
+  let handle = document.getElementById("handle");
+  let table = document.getElementById("table");
+
+  let dx = 2; // displacement factor in x-direction
+  let dy = 2; // displacement factor in y-direction
+  let ballX = 10; // handle x coordinate
+  let ballY = 10; // handle y coordinate
+
+  setInterval(function exec() {
+    ballX += dx;
+    ballY += dy;
+    ball.style.left = `${ballX}px`;
+    ball.style.top = `${ballY}px`;
+    if (ballX > table.offsetWidth - ball.offsetWidth || ballX <= 0) dx *= -1;
+    if (ballY > table.offsetHeight - ball.offsetHeight || ballY <= 0) dy *= -1;
+  }, 1);
+
+  let handley = 0;
+  let dpy = 15; //displacement factor in y-direction
+  document.addEventListener("keydown", (event) => {
+    if (event.keyCode == 38 && handley > 0) {
+      // 38 is uparrow key
+      handley += -1 * dpy;
+    } else if (
+      (event.keyCode = 40 && handley < table.offsetHeight - handle.offsetHeight)
+    ) {
+      // 40 is downArrow key
+      handley += dpy;
+    }
+
+    handle.style.top = `${handley}px`;
+  });
+});
